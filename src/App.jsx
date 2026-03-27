0015ed67f199405d61e5419e2bdb7413a9301f62
Syntax <edubiosamuelvictor@gmail.com>
2026-03-27 21:48:43 +0100

    Working on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,102 @@
-import React, { useState } from 'react'
-import { Sidebar } from './components/Sidebar'
-import StepOne from './components/StepOne'
-import StepTwo from './components/StepTwo'
-import StepThree from './components/StepThree'
+import React, { useState } from "react";
+import { Sidebar } from "./components/Sidebar";
+import StepOne from "./components/StepOne";
+import StepTwo from "./components/StepTwo";
+import StepThree from "./components/StepThree";
 
 const App = () => {
-    const [currentStep, setCurrentStep] = useState(1)
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phoneNumber: '',
-        address: '',
-        age: "",
-        account: '',
-    })
-    const [error, setError] = useState({})
+  const [currentStep, setCurrentStep] = useState(2);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    age: "",
+    account: "",
+  });
+  const [error, setError] = useState({});
 
-    const validate = (step) => {
+  const validate = (step) => {
+    const newError = {};
 
-      const newError = {}
-    
-      if(step === 1) {
-        if(formData.name === ""){
-          newError.name = 'Enter a valid name'
-        }
-        if(!formData.email.includes("@")){
-          newError.email = "Enter a valid name"
-        }
-        if(formData.phoneNumber.length <= 10) {
-          newError.phoneNumber = "Enter a vaild phone number"
-        } 
+    if (step === 1) {
+      if (formData.name === "") {
+        newError.name = "Enter a valid name";
       }
-
-      if (step === 2) {
-        if(formData.address === ""){
-          newError.address = 'Enter an address'
-        }
-        if(formData.age === ""){
-          newError.age = "Enter a valid age"
-        }
-        if(formData.account ===  "") {
-          newError.account = "Enter a vaild account number"
-        } 
-
+      if (!formData.email.includes("@")) {
+        newError.email = "Enter a valid name";
+      }
+      if (formData.phoneNumber.length <= 10) {
+        newError.phoneNumber = "Enter a vaild phone number";
+      }
     }
 
-
-      setError(newError)
-      return Object.keys(newError).length === 0;
+    if (step === 2) {
+      if (formData.address === "") {
+        newError.address = "Enter an address";
+      }
+      if (formData.age === "") {
+        newError.age = "Enter a valid age";
+      }
+      if (formData.account === "") {
+        newError.account = "Enter a vaild account number";
+      }
     }
-    
+
+    setError(newError);
+    return Object.keys(newError).length === 0;
+  };
+
   const handleDisplay = () => {
-        switch (currentStep) {
-        case 1:
-          return <StepOne validate={validate} step={currentStep} setStep={setCurrentStep} formData={formData} setFormData={setFormData} error={error}/>
-        case 2:
-          return <StepTwo validate={validate} step={currentStep} setStep={setCurrentStep} formData={formData} setFormData={setFormData} error={error}/>
-        case 3:
-          return <StepThree validate={validate} step={currentStep} setStep={setCurrentStep} formData={formData} setFormData={setFormData} error={error}/>     
-        default:
-          return null
-      }
+    switch (currentStep) {
+      case 1:
+        return (
+          <StepOne
+            validate={validate}
+            step={currentStep}
+            setStep={setCurrentStep}
+            formData={formData}
+            setFormData={setFormData}
+            error={error}
+          />
+        );
+      case 2:
+        return (
+          <StepTwo
+            validate={validate}
+            step={currentStep}
+            setStep={setCurrentStep}
+            formData={formData}
+            setFormData={setFormData}
+            error={error}
+          />
+        );
+      case 3:
+        return (
+          <StepThree
+            validate={validate}
+            step={currentStep}
+            setStep={setCurrentStep}
+            formData={formData}
+            setFormData={setFormData}
+            error={error}
+          />
+        );
+      default:
+        return null;
     }
+  };
 
   return (
-    <div className='flex justify-center items-center h-screen w-screen'>
-      <div className='flex gap-8 bg-white min-h-125 p-3 rounded-xl'>
-      <Sidebar step={currentStep}/>
-      <div>
-        {handleDisplay()}
-      </div>
+    <div className="md:flex md:justify-center md:items-center h-screen w-screen">
+      <div className="relative md:static md:flex gap-8 bg-[hsl(206_94%_87%)] md:bg-white md:min-h-125 md:p-3 rounded-xl">
+        <Sidebar step={currentStep} />
+        <div className="absolute md:static w-[90%] flex justify-center md:px-0 md:py-0 px-5 py-6 md:my-0 my-20 bg-white z-50 md:z-0 rounded-2xl">
+          {handleDisplay()}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,53 @@
-import React from 'react'
+import React from "react";
 
-export const Sidebar = ({step}) => {
+export const Sidebar = ({ step }) => {
   return (
-    <div className='py-10 px-9 text-white flex flex-col gap-5 bg-[url("./assets/images/bg-sidebar-desktop.svg")] bg-cover bg-no-repeat rounded-2xl w-67.5'>
-        <div className='flex gap-3 items-center'>
-          <p className={step === 1 ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full` : `border font-bold py-3 px-5 rounded-full`}>1</p>
-          <span>
-            <p className='font-medium'>Step 1</p>
-            <p className='font-bold text-xl'>Personal Info</p>
-          </span>
-        </div>
-        <div className='flex gap-3 items-center'>
-          <div className={step === 2 ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full` : `border font-bold py-3 px-5 rounded-full`}>2</div>
-          <span>
-            <p>Step 2</p>
-            <p className='font-bold text-xl'>Address</p>
-          </span>
-        </div>
-        <div className='flex gap-3 items-center'>
-          <div className={step === 3 ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full` : `border font-bold py-3 px-5 rounded-full`}>3</div>
-          <span>
-            <p>Step 3</p>
-            <p className='font-bold text-xl'>Confirmation</p>
-          </span>
-        </div>
+    <div className='absolute md:static z-10 md:py-10 md:px-9 text-white  flex justify-center md:justify-start py-3 items-start md:flex-col md:gap-5 gap-5 bg-[url("./assets/images/bg-sidebar-desktop.svg")] md:bg-cover bg-size-[130%] bg-bottom bg-no-repeat md:h-auto h-[30vh] md:rounded-2xl w-full md:w-67.5'>
+      <div className="flex gap-3 items-center  ">
+        <p
+          className={
+            step === 1
+              ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full`
+              : `border font-bold py-3 px-5 rounded-full`
+          }
+        >
+          1
+        </p>
+        <span className="md:block hidden">
+          <p className="font-medium">Step 1</p>
+          <p className="font-bold text-xl">Personal Info</p>
+        </span>
       </div>
-  )
-}
+      <div className="flex gap-3 items-center">
+        <div
+          className={
+            step === 2
+              ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full`
+              : `border font-bold py-3 px-5 rounded-full`
+          }
+        >
+          2
+        </div>
+        <span className="md:block hidden">
+          <p>Step 2</p>
+          <p className="font-bold text-xl">Address</p>
+        </span>
+      </div>
+      <div className="flex gap-3 items-center">
+        <div
+          className={
+            step === 3
+              ? `bg-[hsl(228_100%_84%)] border-[hsl(228_100%_84%)] text-[hsl(213_96%_18%)] font-extrabold py-3 px-5 rounded-full`
+              : `border font-bold py-3 px-5 rounded-full`
+          }
+        >
+          3
+        </div>
+        <span className="md:block hidden">
+          <p>Step 3</p>
+          <p className="font-bold text-xl">Confirmation</p>
+        </span>
+      </div>
+    </div>
+  );
+};
